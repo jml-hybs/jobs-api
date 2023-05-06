6271c4cc3ac3eac87192b6c22f592f19ec6b4060
unknown <theroyalstradingcorp@gmail.com>
2023-05-06 23:13:07 +0800

    security packages added

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 require('dotenv').config();
 require('express-async-errors');
+
+// exra security packages
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
+
 const express = require('express');
 const app = express();
 //connectDB
@@ -14,8 +21,15 @@ const errorHandlerMiddleware = require('./middleware/error-handler');
 
 
 // extra packages
+app.set('trust proxy', 1)
+app.use(rateLimiter({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+}))
 app.use(express.json());
-
+app.use(helmet());
+app.use(cors());
+app.use(xss())
 // routes
 app.use('/api/v1/jobs', auth, jobsRouter)
 app.use('/api/v1/auth', authRouter)
